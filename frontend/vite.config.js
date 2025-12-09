ff1523449eaf023d1402981f7f0027e5ea3e7a07
aseemfaras <aseemfaras18@gmail.com>
2025-12-09 15:35:32 +0530

    dist dir

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: process.env.VERCEL ? path.resolve(__dirname, '../dist') : 'dist',
+    outDir: 'dist',
     emptyOutDir: true
   },
   server: {
