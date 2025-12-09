53e121b5a9a01901d4ba1577fbbea13bef1f2343
aseemfaras <aseemfaras18@gmail.com>
2025-12-09 15:33:04 +0530

    vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: process.env.VERCEL ? path.resolve(__dirname, '../dist') : 'dist',
+    emptyOutDir: true
+  },
   server: {
     port: 3000,
     proxy: {
